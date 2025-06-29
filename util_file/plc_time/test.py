b5a37bc0f98cf1d71a0ef7470737f46dd6c0272e
Want Sun <1013296326@qq.com>
2025-06-29 21:51:43 +0800

    add

---
+++ util_file/plc_time/test.py
@@ -0,0 +1,26 @@
+import sqlite3
+import secrets
+
+from util_file import settings
+
+DATA_FILE = settings.DATA_DIR.joinpath('20250628-NDEF-client.log')
+
+
+def read_file(filepath: str):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        for line in f:
+            yield line.rstrip('\n')
+
+
+def get_data():
+    with open('error.log', 'w', encoding='utf-8') as out_file:
+        for line in read_file(DATA_FILE):
+            if '耗时' not in line:
+                continue
+            print(line)
+            out_file.write(line + '\n')
+
+
+
+if __name__ == '__main__':
+    get_data()
---
+++ util_file/settings.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+DEV_MODE = os.getenv('DEBUG') == '1'
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DB_DIR = BASE_DIR / 'dbs'
+
+DATA_DIR =  BASE_DIR / 'files'
