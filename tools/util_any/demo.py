330bfedbde17a79eada3965ed0536d6de6e7c3fa
wangxun <1013296326@qq.com>
2025-08-21 16:49:24 +0800

    add

--- tools/util_any/demo.py
+++ tools/util_any/demo.py
@@ -2,6 +2,7 @@
 will
 """
 
+##
 # 判断字符串 "ac" 是否出现在 列表 任意元素的"子串"中
 lst = ["foo", "bar", "abc", "xyz"]
 target = "ac"
@@ -9,6 +10,14 @@ target = "ac"
 exists = any(target in s for s in lst)
 print(exists)  # False 因为没有元素包含 "ac"
 
+##
 # 忽略大小写
 exists_ci = any(target.lower() in s.lower() for s in lst)
 
+##
+# 先过滤
+lst = ["foo", None, 123, "bacon"]
+target = "ac"
+
+exists = any(target in str(s) for s in lst if s is not None)
+print(exists)  # True（"bacon" 包含 "ac"）
